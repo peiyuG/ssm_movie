6712e0fd2699c41835180c46b5ec6e3dc9e9fb31
wj <1634896520@qq.com>
2018-04-09 16:50:42 +0800

    movie接口，代码规范

--- src/main/java/service/movie/MovieService.java
+++ src/main/java/service/movie/MovieService.java
@@ -12,20 +12,20 @@ public interface MovieService {
 	/*
 	 * 根据不同类型返回电影pojo集合
 	 */
-	public Status getMovieByDifferentType(String category, 
+	Status getMovieByDifferentType(String category, 
 			String country, String release_time, int is_vip_movie,String otherType);
    
 	/*
 	 * 根据电影名返回同名的该类电影的pojo集合
 	 */
-	public Status getMovieByname(String name);
+	Status getMovieByname(String name);
 	/*
 	 * 返回点击量最大的部分电影pojo集合
 	 */
-	public Status getRanklist();
+	Status getRanklist();
     
     /*
 	 * 根据电影ID返回该电影的单个pojo
 	 */
-	public Status getMovieByID(int id);
+	Status getMovieByID(int id);
 }
